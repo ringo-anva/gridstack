cb365de1953b1c7a7bf1a6ac85e994860666dc60
Ringo <ringob@anva.nl>
2023-11-23 11:37:16 +0100

    adding gridstack example

--- apps/gridstackmf/remote1/src/app/remote-entry/nx-welcome.component.ts
+++ apps/gridstackmf/remote1/src/app/remote-entry/nx-welcome.component.ts
@@ -10,6 +10,7 @@ import {
   GridstackComponent,
   GridstackModule,
   NgGridStackWidget,
+  NgGridStackOptions,
 } from 'gridstack/dist/angular';
 import { GridStackOptions } from 'gridstack';
 
@@ -26,17 +27,35 @@ export class NxWelcomeComponent {
   @ViewChild(GridstackComponent)
   public readonly gsComp?: GridstackComponent;
 
-  @Input()
-  public options!: GridStackOptions;
-  public readonly parent = inject(NxWelcomeComponent, {
-    optional: true,
-    skipSelf: true,
-  });
+  // @Input()
+  // public options!: GridStackOptions;
 
-  public addComponent(widget: NgGridStackWidget): NgGridStackWidget {
-    if (!this.gsComp?.grid) throw Error('add component was called to early');
-    const addWidget = this.gsComp.grid.addWidget(widget);
-    // this.changed.next();
-    return addWidget;
-  }
+  // public readonly parent = inject(NxWelcomeComponent, {
+  //   optional: true,
+  //   skipSelf: true,
+  // });
+
+  // there's also NgGridStackOptions
+  public options: GridStackOptions = {
+    column: 6,
+    cellHeight: 50,
+    margin: 5,
+    minRow: 1, // don't collapse when empty
+    removable: '.trash',
+    acceptWidgets: true,
+    float: true,
+    children: [
+      { x: 0, y: 0, w: 2, h: 2, content: 'cell 1 remote 1' },
+      { x: 3, y: 1, h: 2 },
+      { x: 4, y: 1 },
+      { x: 2, y: 3, w: 3, maxW: 3, id: 'special', content: 'has maxW=3' },
+    ],
+  };
+
+  // public addComponent(widget: NgGridStackWidget): NgGridStackWidget {
+  //   if (!this.gsComp?.grid) throw Error('add component was called to early');
+  //   const addWidget = this.gsComp.grid.addWidget(widget);
+  //   // this.changed.next();
+  //   return addWidget;
+  // }
 }
